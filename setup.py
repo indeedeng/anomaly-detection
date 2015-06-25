005702784d5b02e6d568526fcdd97d4ce08a18d9
Jingchen Liu <liu@indeed.com>
2015-06-25 15:23:41 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 def read(fname):
@@ -16,15 +16,15 @@ setup(
     name="anomaly-detection",
     version="0.0.1",
     description="A python implementation of https://github.com/twitter/AnomalyDetection",
-    packages=find_packages(exclude=['tests.*']),
+    py_modules=['anoms'],
     install_requires=[
         'pandas>=0.12.0',
         'scipy>=0.15.1',
         'numpy>=1.9.2',
-        'pyloess==master'
+        'pyloess==0.0.0'
     ],
     dependency_links=[
-        'https://github.com/andreas-h/pyloess/tarball/7415090e00c3987eecc44be2efcfbdaf038656e0#egg=pyloess-master'
+        'https://github.com/andreas-h/pyloess/tarball/7415090e00c3987eecc44be2efcfbdaf038656e0#egg=pyloess-0.0.0'
     ],
     long_description=read('README.md'),
 )
